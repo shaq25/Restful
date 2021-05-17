23137e7751ae8aaf0b3abc25172611f5339bdff8
Shaqueal Carroll <shaqueal66@gmail.com>
2021-05-17 11:19:58 -0400

    Parsing Request Paths

--- index.js
+++ index.js
@@ -5,11 +5,23 @@
 // Dependencies
 
 var http =require('http');
+var url =require('url');
 
 // This servers should respond to all request with a string
 var server = http.createServer(function(req, res){
 
-  res.end('Hello World \n');
+// Get URL and parse it
+var parseUrl= url.parse(req.url,true);
+
+//Get the Path
+var path=parseUrl.pathname;
+var trimmedPath= path.replace(/^\/+|\/+$/g, '');
+
+res.end('Hello World \n');
+
+//Log the request
+console.log("Request received on Path: "+ trimmedPath);
+
 
 
 });
